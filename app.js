666c3f842767250951dd9c4ad9bdd7e4366d0430
MKA1500 <maciejkabsch@gmail.com>
2018-03-25 19:23:29 +0200

    node tests

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-console.log('starting app.');
+console.log('starting app.js');
 
 const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js');
 
-fs.appendFile('greetings.txt', 'Hello World!', function(err){
+var res = notes.addNote();
+console.log(res);
+
+var user = os.userInfo();
+
+fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`, function(err){
   if(err){
     console.log('Unable to write to file');
   }
---
+++ notes.js
@@ -0,0 +1,6 @@
+console.log('starting notes.js');
+module.exports.age = 25;
+module.exports.addNote = () => {
+  console.log('addNote');
+  return 'new note';
+}
